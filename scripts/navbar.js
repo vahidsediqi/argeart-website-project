5b4e2b04e9beddc082aa0fb5807bc60dccd16e14
vahidsediqi <vsediqi@live.com>
2021-12-15 11:34:50 -0800

    some revesion added

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -12,3 +12,33 @@ btn.addEventListener('click', function(e) {
   
 })
 
+
+// var rect = $('#moveit')[0].getBoundingClientRect();
+// var mouse = {x: 0, y: 0, moved: false};
+
+// $("#moveit").mousemove(function(e) {
+//   mouse.moved = true;
+//   mouse.x = e.clientX - rect.left;
+//   mouse.y = e.clientY - rect.top;
+// });
+ 
+// // Ticker event will be called on every frame
+// TweenLite.ticker.addEventListener('tick', function(){
+//   if (mouse.moved){    
+//     parallaxIt(".slide", -100);
+//     parallaxIt("img", -30);
+//   }
+//   mouse.moved = false;
+// });
+
+// function parallaxIt(target, movement) {
+//   TweenMax.to(target, 0.5, {
+//     x: (mouse.x - rect.width / 2) / rect.width * movement,
+//     y: (mouse.y - rect.height / 2) / rect.height * movement
+//   });
+// }
+
+// $(window).on('resize scroll', function(){
+//   rect = $('#container')[0].getBoundingClientRect();
+// })
+
