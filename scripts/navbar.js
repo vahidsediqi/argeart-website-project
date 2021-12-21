3450a796f056c301ea6802c52318f8c91de5dcc7
unknown <you@example.com>
2021-12-21 14:59:56 +0300

    contact page

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -42,3 +42,14 @@ btn.addEventListener('click', function(e) {
 //   rect = $('#container')[0].getBoundingClientRect();
 // })
 
+// contact page
+
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
